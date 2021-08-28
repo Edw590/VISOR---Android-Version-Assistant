1b5463d4b2681f2cc5b7b8c08a4742a1448f7dac
DADi590 <40151365+DADi590@users.noreply.github.com>
2021-08-28 17:53:26 +0100

    Update: system app installation for all APIs
    1.1;
    ===
    List of changes:
    - Update: system app installation for all APIs.
      Added the correct way of installing a system app on API18- and on
      API19-20. For API21+ it's the way that was already being used.
    
    ===

--- Install_system_Windows_allAPI.bat
+++ Install_system_Windows_allAPI.bat
@@ -1,5 +1,12 @@
 ::WIN BATCH SCRIPT
 
+:: 0 for API18-; 1 for API19-20; 2 for API21+
+set API_type=%1
+if [%1] == [] (
+	echo ERROR - No API type argument
+	exit /B 1
+)
+
 :: CHANGE THESE
 set app_package=com.dadi590.assist_c_a
 set dir_app_name=%app_package%
@@ -8,10 +15,18 @@ set dir_app_name=%app_package%
 set ADB="adb.exe"
 ::ADB_SH="%ADB% shell" # this script assumes using `adb root`. for `adb su`
 
-set path_sysapp=/system/priv-app
+if [%1] == "0" (
+	set path_sysapp=/system/app
+) else (
+	set path_sysapp=/system/priv-app
+)
 set apk_host=.\app\build\outputs\apk\debug\app-debug.apk
 set apk_name=%dir_app_name%.apk
-set apk_target_dir=%path_sysapp%/%dir_app_name%
+if [%1] == "2" (
+	set apk_target_dir=%path_sysapp%/%dir_app_name%
+) else (
+	set apk_target_dir=%path_sysapp%
+)
 set apk_target_sys=%apk_target_dir%/%apk_name%
 
 :: Delete previous APK
@@ -28,7 +43,9 @@ set ADB_SH=%ADB% shell su -c
 %ADB_SH% mkdir -p /sdcard/tmp
 %ADB_SH% mkdir -p %apk_target_dir%
 %ADB% push %apk_host% /sdcard/tmp/%apk_name%
-%ADB_SH% mv /sdcard/tmp/%apk_name% %apk_target_sys%
+:: Use cp and rm instead of mv because of a possible error (happened on miTab Advance): "failed on (...) - Cross-device link", happening because of moving across devices, it seems.
+%ADB_SH% cp /sdcard/tmp/%apk_name% %apk_target_sys%
+%ADB_SH% rm /sdcard/tmp/%apk_name%
 %ADB_SH% rmdir /sdcard/tmp
 
 :: Give permissions
